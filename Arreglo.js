3a10f4c690b3a34b5e1e65ad53d21736d19fbe38
VictorRios17 <victor1ariel2rios3martinez45@gmail.com>
2026-09-03 00:34:27 -0700

    Add files via upload

---
+++ Arreglo.js
@@ -0,0 +1,8 @@
+listanum = [1, 2, 3, 4, 5, 6, 7];
+console.log(listanum.join(""))
+
+//join sirve para mostrar de una manera una arreglo con algun acomodo como una coma o sin espacio como se ve aqui
+
+
+//el join mas que nada lo puse ya que si se lo quitaba me mostraba como una parte arriba y otra abajo como tabla y queria que me los mostrara 
+//corrido asi que investigue y por eso use el join
