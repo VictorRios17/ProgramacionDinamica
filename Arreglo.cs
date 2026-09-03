09e3f7ff9c9d4df7b402cdc96f3806eb7a2fb74a
VictorRios17 <victor1ariel2rios3martinez45@gmail.com>
2026-09-03 01:04:28 -0700

    Add files via upload

---
+++ Arreglo.cs
@@ -0,0 +1,6 @@
+using System;
+
+int[] numeros = {1, 2, 3, 4, 5, 6, 7};
+Console.WriteLine(string.Join(" " , numeros));
+
+//era con el join que era unirlos o usar un for pero preferi el join por que se me hizo mas sencillo espero este bien
